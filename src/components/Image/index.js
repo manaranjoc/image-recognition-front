11859a02871a1e2237745470b014a634354e7001
Miguel Angel Naranjo <miguelangelnaranjo@talos.local>
2021-02-11 20:07:03 -0500

    Add bounding box recognition

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,8 +1,13 @@
 import React, {useEffect, useState, useRef} from "react";
+import styles from './Image.module.css'
 
 const Image = ({image, labels}) => {
 
+    const width = 500;
+    const height = 500;
+
     const imageContainer = useRef();
+    const [boundingBoxes, setBoundingBoxes] = useState([]);
 
     useEffect(() => {
         if (image !== undefined) {
@@ -15,11 +20,34 @@ const Image = ({image, labels}) => {
         }
     }, [image])
 
-    return (
-        <div className="imageWrapper">
-            <img ref={imageContainer} src="" alt=""/>
-            <div className="boundingBoxes">
+    useEffect(() => {
+        if (labels !== undefined) {
+            setBoundingBoxes(labels.filter((label) => label.Instances.length));
+        }
+    }, [labels])
 
+    return (
+        <div className={styles.imageWrapper}>
+            <img ref={imageContainer} src="" alt="" width={width} height={height}/>
+            <div className="boundingBoxes">
+                {
+                    boundingBoxes.map((label) => {
+                        const boundingBoxesInstances = label.Instances;
+
+                        return boundingBoxesInstances.map(({BoundingBox}) => {
+                            const position = {
+                                top: BoundingBox.Top * height,
+                                left: BoundingBox.Left * width,
+                                width: BoundingBox.Width * width,
+                                height: BoundingBox.Height * height,
+                                border: `2px #${Math.floor(Math.random()*16777215).toString(16)} solid`,
+                            }
+
+                            return (<div key={label.Name} className={styles.boundingBox} style={position}></div>)
+                        })
+
+                    })
+                }
             </div>
         </div>
     )
